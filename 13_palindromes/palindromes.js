2e225e1719a79b2fd772fc06983b5a76a37802a3
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-12-02 16:17:10 +0000

    finished palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = '0123456789abcdefghijklmnopqrstuvwxyz';
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    return reversedStr === cleanedStr;
 };
 
 // Do not edit below this line
