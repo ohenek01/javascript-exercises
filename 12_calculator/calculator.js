3f88dba4da72441f98fbc2201edd94c2a845041a
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-12-02 14:01:53 +0000

    finished exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return result = a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return result = a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => 
+    acc + curr, 0
+  )
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => 
+    acc * curr
+  )
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let j = 1
+  for(i = n; i > 0; i--){
+    j *= i;
+  }
+  return j
 };
 
 // Do not edit below this line
