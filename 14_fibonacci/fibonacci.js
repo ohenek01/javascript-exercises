6b42d285b91f40ddfd3d9c74d4d018d23fd0d651
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-12-02 18:31:49 +0000

    finished the fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num)
+    if(num == 1 || num == 0){
+        return num
+    }
+    else if(num < 0){
+        return "OOPS";
+    }
+    return result = fibonacci(num - 1) + fibonacci(num - 2)
 };
 
 // Do not edit below this line
