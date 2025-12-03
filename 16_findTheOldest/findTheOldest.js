26f8d817707ca427a873e00e88286f4944c938b3
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-12-03 14:18:52 +0000

    completed findTheOldestAge exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(birth, death){
+        if(!death){
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
 
+    return people
+    .reduce((oldestAge, currAge) => {
+        const oldestPerson = getAge(oldestAge.yearOfBirth, oldestAge.yearOfDeath);
+        const currPerson = getAge(currAge.yearOfBirth, currAge.yearOfDeath);
+
+        return oldestPerson > currPerson ? oldestAge : currAge;
+    })
+    
 };
 
 // Do not edit below this line
